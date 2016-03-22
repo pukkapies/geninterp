e083b80f245f5e43df5e8e9c1e7bd2477d088ed3
pukka_pies <knwebster@me.com>
2016-03-22 11:55:40 +0100

    Make setup.py, delete __init__.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='geninterp',
+      version='0.1',
+      description='Generalised interpolation using kernel regression',
+      url='https://github.com/pukkapies/geninterp',
+      author='Kevin Webster',
+      author_email='knwebster@gmail.com',
+      license='NA',
+      packages=['geninterp'],
+      zip_safe=False)
